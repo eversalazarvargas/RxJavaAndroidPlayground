ed60674293ccf25ac805ef24ac07ac548ae36b83
everardo salazar <>
2019-10-28 21:51:31 -0700

    more

--- app/src/main/java/com/example/everardo/rxjavaplayground/activity/FlickrActivity.kt
+++ app/src/main/java/com/example/everardo/rxjavaplayground/activity/FlickrActivity.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import android.util.Log
 import com.example.everardo.rxjavaplayground.R
 import com.example.everardo.rxjavaplayground.model.FlickrPhotoInfoResponse
+import com.example.everardo.rxjavaplayground.model.FlickrPhotosGetSizesResponse
 import com.example.everardo.rxjavaplayground.model.FlickrSearchResponse
+import com.example.everardo.rxjavaplayground.model.Photo
 import com.example.everardo.rxjavaplayground.network.FlickrApi
 import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
 import kotlinx.android.synthetic.main.activity_flickr.*
 
 class FlickrActivity: Activity() {
@@ -31,20 +34,31 @@ class FlickrActivity: Activity() {
                             flickrApi.searchPhotos(text, 10)
                         })
 
+
         //text expand
-        val photoInfo: Observable<FlickrSearchResponse.Photo> =
-                searchPhotosObservable
-                        .flatMap({ photoList ->
-                            Observable.fromIterable(photoList)
+        val photosWithThumbnail: Observable<List<Photo>> =
+                searchPhotosObservable.flatMap({ photoList ->
+                    Observable.fromIterable(photoList)
+                            .concatMap({ photo ->
+                                Observable.combineLatest(flickrApi.photoInfo(photo.id),
+                                        flickrApi.getSizes(photo.id),
+                                        BiFunction<FlickrPhotoInfoResponse, FlickrPhotosGetSizesResponse, Photo> { photoInfo, photoSizes ->
+                                            Photo.createPhoto(photoInfo.photoInfo, photoSizes.sizes)
+                                        })
+                            })
+                            .toList()
+                            .toObservable()
+                })
 
-                        })
-
-        photoInfo
+        photosWithThumbnail
                 .subscribeOn(AndroidSchedulers.mainThread())
-                .subscribe({ photo ->
-                    Log.i("PROBANDO", "photo = ${photo.title}")
+                .subscribe({ photos ->
+                    photos.forEach { photo ->
+                        Log.i("PROBANDO", "photo = ${photo.title} thumbnail = ${photo.thumbnailUrl}")
+                    }
+
                 },
-                        {error -> Log.e("PROBANDO", "error= ${error.message}")})
+                        { error -> Log.e("PROBANDO", "error= ${error.message}") })
 
 
     }
